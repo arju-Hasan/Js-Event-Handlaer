227c36e6a9f74bc99b51d414f22cb6f763d9081e
Arju-Hasan <rajuahmeed698@gmail.com>
2025-08-22 15:30:47 +0600

    add a morea event html and js file

--- js/more-event.js
+++ js/more-event.js
@@ -1,1 +1,47 @@
-console.log("this is my more event js file adding ");
+// document
+//   .getElementById("btn-mouse")
+//   .addEventListener("mouseenter", function () {
+//     console.log("mouse tiger");
+//   });
+// 
+// 
+//   document
+//     .getElementById("btn-mouse")
+//     .addEventListener("mousemove", function () {
+//       console.log("mouse tiger");
+//     });
+
+    document
+      .getElementById("btn-mouse")
+      .addEventListener("mouseout", function () {
+        console.log("mouse tiger");
+      });
+
+
+
+      document
+        .getElementById("your-name")
+        .addEventListener("focus", function () {
+          console.log("User write the name ");
+        });
+
+      document
+        .getElementById("your-email")
+        .addEventListener("focus", function () {
+          console.log("User write the email ");
+        });
+
+      document
+        .getElementById("your-name")
+        .addEventListener("blur", function () {
+          console.log("User done the name ");
+        });
+
+      document
+        .getElementById("your-email")
+        .addEventListener("blur", function (e) {
+          console.log("User done the email");
+          if (e.target.value.trim() !== "") {
+            e.target.setAttribute("readonly", true);  // akber liklea r kiltea parbea na
+          }
+        });
